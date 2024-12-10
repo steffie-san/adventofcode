47fff290ae0b7b1377857a974728cf29a687d408
Stefan Nilsson <no@no.no>
2024-12-10 21:32:49 +0100

    Finished 3-2

--- 2024/Lua/Day3.lua
+++ 2024/Lua/Day3.lua
@@ -3,8 +3,9 @@ require("Utils")
 
 function ExecuteDay3(doStar1, doStar2)
     local input = ReadInput(3)
-    local count = 0
     local sum = 0
+    local secondSum = 0
+    local doProcess = true
     for k, v in ipairs(input) do
         print("doint line " .. k)
         local offset = 0
@@ -14,21 +15,43 @@ function ExecuteDay3(doStar1, doStar2)
             local left
             local right
             start, stop, left, right = string.find(v, "mul%((%d%d?%d?)%,(%d%d?%d?)%)", offset)
-            print(start, stop, left, right)
+
+            local nextDo = string.find(v, "do%(%)", offset)
+            local nextDont = string.find(v, "don%'t%(%)", offset)
+
+            if start == nil then
+                break
+            end
+
+            if nextDo ~= nil and nextDo < start then
+                if nextDont ~= nil and nextDont < nextDo then
+                    doProcess = false
+                    offset = nextDont + 1
+                    goto continue
+                end
+                doProcess = true
+                offset = nextDo + 1
+                goto continue
+            end
+            if nextDont ~= nil and nextDont < start then
+                doProcess = false
+                offset = nextDont + 1
+                goto continue
+            end
+
             if start ~= nil and stop ~= nil then
                 offset = stop
-                count = count + 1
                 left = tonumber(left)
                 right = tonumber(right)
-                sum = sum + left * right
+                local toAdd = left * right
+                sum = sum + toAdd
+                if doProcess then
+                    secondSum = secondSum + toAdd
+                end
             end
-            if start == nil then break end
-
-            if stop ~= nil then
-                offset = stop
-            end
+            ::continue::
         end
     end
-    print(count)
-    print("The answer for star 3-1 is", sum)
+    if doStar1 then print("The answer for star 3-1 is", sum) end
+    if doStar2 then print("The answer for star 3-2 is", secondSum) end
 end
