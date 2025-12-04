a09f560e23d29c4fbdb56cd94fca99f50913e371
Steffie Nilsson <feffef.nosslin@gmail.com>
2025-12-04 20:26:34 +0100

    Added 2025 base files

---
+++ 2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace adventofcode_2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
