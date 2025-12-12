386bfe15521cefffa87a6a4f9bb1e35e4d3813b5
Stefan Nilsson <stefan.nilsson@piktiv.se>
2025-12-12 12:56:25 +0100

    Attempt at 7-2

--- 2025/Day7.cs
+++ 2025/Day7.cs
@@ -4,6 +4,7 @@ namespace adventofcode_2025
 {
     internal class Day7 : IDay
     {
+        Dictionary<(int, int), int> splitterHitCount = new();
         public void Execute(string input, out string star1, out string star2)
         {
             int splitCounter = 0;
@@ -36,7 +37,46 @@ namespace adventofcode_2025
             }
 
             star1 = splitCounter.ToString();
-            star2 = "invalid";
+
+            for (int i = 0; i < rawRows.Length; i++)
+            {
+                rows[i].Clear();
+                rows[i].Append(rawRows[i]);
+                //Console.WriteLine(rows[i]);
+            }
+            int realities = TraverseRecursively(rows, rawRows[0].IndexOf('S'), 1);
+
+            star2 = realities.ToString();
+        }
+
+        int TraverseRecursively(StringBuilder[] rows, int x, int y)
+        {
+            //Console.WriteLine($"start traversal at ({x},{y})");
+            int result = 0;
+            while (y < rows.Length)
+            {
+                char c = rows[y][x];
+                if (c == '^')
+                {
+                    if (splitterHitCount.TryGetValue((x, y), out int count)) count++;
+                    else count = 1;
+                    splitterHitCount[(x, y)] = count;
+
+                    //Console.WriteLine($"Hit splitter at ({x},{y}) (hitcount: {count})");
+                    result += TraverseRecursively(rows, x - 1, y);
+                    result += TraverseRecursively(rows, x + 1, y);
+                    return result;
+                }
+                else if (c == '|' || c == '.')
+                {
+                    //Console.WriteLine($"({x},{y}) is traversable ({c})");
+                    rows[y][x] = '|';
+                    y++;
+                }
+                //else Console.WriteLine($"unknown char {c} at ({x},{y})");
+            }
+            Console.WriteLine("Line terminated");
+            return 1;
         }
     }
 }
