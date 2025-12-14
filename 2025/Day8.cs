72bddb10c580b52d4f6dd3d4a3c86625772b2a69
Steffie Nilsson <feffef.nosslin@gmail.com>
2025-12-14 11:22:47 +0100

    Added day 8 files

---
+++ 2025/Day8.cs
@@ -0,0 +1,13 @@
+﻿namespace adventofcode_2025
+{
+    internal class Day8 : IDay
+    {
+        public void Execute(string input, out string star1, out string star2)
+        {
+
+
+            star1 = "invalid";
+            star2 = "invalid";
+        }
+    }
+}
--- 2025/Program.cs
+++ 2025/Program.cs
@@ -15,10 +15,11 @@
                 new Day5(),
                 new Day6(),
                 new Day7(),
+                new Day8(),
             };
 
-            bool useExampleInput = false;
-            int dayIndex = 6;
+            bool useExampleInput = true;
+            int dayIndex = 7;
 
             string result1, result2;
             days[dayIndex].Execute(GetInput(dayIndex + 1, useExampleInput), out result1, out result2);
